841b7a842072009266d6fc2a8b05315552a5916e
LeYohan-rgb <williamotonielevertz@gmail.com>
2026-05-29 17:27:51 -0400

    Farmet Fight Initial Menu Started

---
+++ Scripts/Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var player_name_1 = "Jerónimo"
+var player_name_2 = "Emiliano"
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 
-#ADDED FIRST SCRIPTT
-func hello():
-	print("test to see if Hackatime and Github commiting work! :D")
+@onready var farmer_frenzy_mode = $farmer_frenzy
+@onready var main_menu_layout = $main
+
+
+func _on_farmer_fight_button_pressed() -> void:
+	farmer_frenzy_mode.begin_game()
+	main_menu_layout.hide()
+	farmer_frenzy_mode.show()
+	
